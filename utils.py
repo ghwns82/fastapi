cd067ab15a3e4cc9285d0a0eb29802a7d005a660
leehojun <junvery2@naver.com>
2025-10-30 05:32:31 +0900

    d

--- utils.py
+++ utils.py
@@ -3,18 +3,19 @@ import os
 from dotenv import load_dotenv
 
 import insightface
+from insightface.app import FaceAnalysis
 from pinecone import Pinecone
 
 # 로딩
 
 @lru_cache(maxsize=1)
 def get_models(ctx_id: int = -1):
-    print('모델 설치 중')
-    model_pack_name = 'buffalo_l'
-    app = FaceAnalysis(name=model_pack_name)
+    # print('모델 설치 중')
+    # model_pack_name = 'buffalo_l'
+    # app = FaceAnalysis(name=model_pack_name)
     print('모델 로딩 중')
-    det = insightface.model_zoo.retinaface.RetinaFace("buffalo_l/det_10g.onnx")
-    rec = insightface.model_zoo.arcface_onnx.ArcFaceONNX("buffalo_l/w600k_r50.onnx")
+    det = insightface.model_zoo.retinaface.RetinaFace("model/det_10g.onnx")
+    rec = insightface.model_zoo.arcface_onnx.ArcFaceONNX("model/w600k_mbf.onnx")
     det.prepare(ctx_id=ctx_id)  # CPU:-1, GPU:0
     rec.prepare(ctx_id=ctx_id)
     print('모델 로딩 완료')
